5009a3a75ce565141424ff394e100404e41b3474
Swarmz <gavinsutton6511@gmail.com>
2024-10-21 12:21:31 +0900

    最終行は空行を入れる, 文字列展開を削除する

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,24 +1,20 @@
-def fizzbuzz
-  for i in 1..20
-    puts case 
-      when i % 3.0 == 0 && i % 5.0 == 0 then "fizzbuzz"
-      when i % 3.0 == 0 then "fizz"
-      when i % 5.0 == 0 then "buzz"
-      else "#{i}"
-    end
+for i in 1..20
+  puts case 
+    when i % 3.0 == 0 && i % 5.0 == 0 then "fizzbuzz"
+    when i % 3.0 == 0 then "fizz"
+    when i % 5.0 == 0 then "buzz"
+    else i
   end
-
-  # for i in 1..20
-  #   if i % 3.0 == 0 && i % 5.0 == 0
-  #     puts "fizzbuzz"    
-  #   elsif i % 3.0 == 0
-  #     puts "fizz"
-  #   elsif i % 5.0 == 0
-  #     puts "buzz"
-  #   else
-  #     puts "#{i}"
-  #   end
-  # end
 end
 
-fizzbuzz
+# for i in 1..20
+#   if i % 3.0 == 0 && i % 5.0 == 0
+#     puts "fizzbuzz"    
+#   elsif i % 3.0 == 0
+#     puts "fizz"
+#   elsif i % 5.0 == 0
+#     puts "buzz"
+#   else
+#     puts "#{i}"
+#   end
+# end
