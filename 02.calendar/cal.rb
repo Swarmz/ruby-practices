45753b016a21ee75e22ce17e3564fe3f1b1fcc68
Swarmz <gavinsutton6511@gmail.com>
2024-10-24 13:21:15 +0900

    4回目の提出修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -4,7 +4,7 @@ require 'optparse'
 
 def main
   year, month = get_year_month
-  puts "#{month}月 #{year}".rjust(8 + month.digits.length + year.digits.length)
+  puts "#{month}月 #{year}".center(15 + month.digits.length + year.digits.length)
   puts "日 月 火 水 木 金 土"
   print_cal(year, month)
 end
@@ -22,12 +22,12 @@ def get_year_month
 end
 
 def print_cal(year, month)
-  first_day = Date.new(year, month, 1)
-  last_day = Date.new(year, month, -1)
-  print "".rjust(first_day.wday * 3) 
-  (first_day..last_day).each do |date|
+  first_date = Date.new(year, month, 1)
+  last_date = Date.new(year, month, -1)
+  print "   " * first_date.wday  
+  (first_date..last_date).each do |date|
     print "\n" if date.sunday? && date.day != 1 # 土曜日を表示した後に改行を入れる
-    print "#{date.day}".center(3)
+    print "#{date.day}".rjust(2) + " "
   end
 end
 
