4af6a79e7ea95a0709e68a9eed1cbfedf63dc93e
Swarmz <gavinsutton6511@gmail.com>
2024-10-30 13:46:49 +0900

    3回目の提出修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,8 +1,9 @@
 score = ARGV[0]
 scores = score.split(',')
 shots = []
+frames_one_to_nine = 9 * 2
 scores.each do |s|
-  if s == 'X' && shots.length < (2 * 9)
+  if s == 'X' && shots.length < frames_one_to_nine
     shots.push(10, 0)
   elsif s == 'X' # 10フレーム目の場合 例：[10, 10, 10］
     shots << 10
@@ -11,25 +12,19 @@ scores.each do |s|
   end
 end
 
-frames = shots[0...18].each_slice(2).to_a # 0～9フレーム
-frames << shots[18..]
+frames = shots[...frames_one_to_nine].each_slice(2).to_a
+frames << shots[frames_one_to_nine..]
 frames.push([0, 0], [0, 0]) # each_consを10フレーム目まで継続させる
 
-point = frames.each_cons(3).with_index.sum do |(frame, next_frame, next_next_frame), frame_num|
+point = frames.each_cons(3).with_index(1).sum do |(frame, next_frame, next_next_frame), frame_num|
   frame.sum +
-    if frame_num == 9 # 10フレーム目かどうかのチェック
+    if frame_num == 10
       0
     elsif frame[0] == 10
-      if next_frame[0] == 10
-        if next_next_frame[0] == 10
-          20
-        elsif frame_num == 8 # 9フレーム目がdoubleの場合
-          next_frame[0..1].sum
-        else
-          10 + next_next_frame[0]
-        end
-      else
+      if frame_num == 9 || next_frame[0] != 10 # 9フレーム目がダブルの場合のため ||
         next_frame[0..1].sum
+      else
+        10 + next_next_frame[0]
       end
     elsif frame[0..1].sum == 10
       next_frame[0]
