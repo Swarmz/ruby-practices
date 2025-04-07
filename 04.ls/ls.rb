dc8ae31525329d0be7c0c38e84a2ecce845b7056
gavin <gavinsutton6511@gmail.com>
2025-04-07 20:07:19 +0900

    Add option to show files in reverse order

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -15,7 +15,10 @@ def handle_args
   files = Dir.new(Dir.pwd).each_child.filter_map { |file| file unless file.start_with?('.') }
   parser = OptionParser.new
   parser.on('-a', '--all', 'Show all files, including those that start with .') do
-    files = Dir.new(Dir.pwd)
+    files = Dir.new(Dir.pwd).sort
+  end
+  parser.on('-r', '--reverse', 'Show files in reverse order') do
+    files = files.sort.reverse
   end
   begin
     parser.parse!
@@ -30,7 +33,7 @@ end
 def arrange_files(files)
   rows = files.count.ceildiv(COLUMNS)
   # transpose メソッドを使用するには、行と列を入れ替えることができるように、1-2個の数字しか持たない配列を空白で埋める
-  split_files = files.sort.each_slice(rows).to_a.tap { |array| array.last.fill(' ', array.last.length, rows - array.last.length) }
+  split_files = files.each_slice(rows).to_a.tap { |array| array.last.fill(' ', array.last.length, rows - array.last.length) }
   split_and_spaced_files = split_files.map do |row|
     max_char_length = row.max_by(&:length).length
     row.map do |file_name|
